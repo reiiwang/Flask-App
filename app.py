3ce4f9b6161c70326403025bf4ebbc815257f59a
reiiwang <sqq9458@gmail.com>
2024-07-24 01:49:37 +0800

    frontend

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
+from data.question import get_questions
+
+questions = get_questions()
 
 app = Flask(__name__)
-
-questions = [
-    {
-        "text": "Question 1: What is your favorite color?",
-        "options": ["Red", "Blue", "Green", "Yellow"]
-    },
-    {
-        "text": "Question 2: Which season do you prefer?",
-        "options": ["Spring", "Summer", "Autumn", "Winter"]
-    },
-    {
-        "text": "Question 3: What's your preferred mode of transportation?",
-        "options": ["Car", "Bicycle", "Public Transport", "Walking"]
-    },
-    {
-        "text": "Question 4: Which cuisine do you enjoy the most?",
-        "options": ["Italian", "Chinese", "Mexican", "Indian"]
-    }
-]
 
 @app.route('/')
 def index():
     return render_template('index.html', questions=questions)
+
+@app.route('/lastpage')
+def lastpage():
+    return render_template('lastpage.html', title='Thank You')
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -39,12 +27,16 @@ def submit():
         # Here you could save the responses to a database
         # For this example, we'll just print them
         print(responses)
+        # save to file
+        with open('responses.txt', 'a') as f:
+            f.write(str(responses) + '\n')
         
         # You could also pass the responses to a template to display them
         return render_template('results.html', responses=responses)
     
     # If not a POST request, redirect to the index
-    return redirect(url_for('index'))
+    # return redirect(url_for('index'))
+    return redirect(url_for('lastpage'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data/question.py
@@ -0,0 +1,21 @@
+questions = [
+    {
+        "text": "Question 1: What is your favorite color?",
+        "options": ["Red", "Blue", "Green", "Yellow"]
+    },
+    {
+        "text": "Question 2: Which season do you prefer?",
+        "options": ["Spring", "Summer", "Autumn", "Winter"]
+    },
+    {
+        "text": "Question 3: What's your preferred mode of transportation?",
+        "options": ["Car", "Bicycle", "Public Transport", "Walking"]
+    },
+    {
+        "text": "Question 4: Which cuisine do you enjoy the most?",
+        "options": ["Italian", "Chinese", "Mexican", "Indian"]
+    }
+]
+
+def get_questions():
+    return questions
