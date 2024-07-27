4b2dcd61eba2e35db0bdc444de1f1c8f68a2b214
Rei-I Wang <79686598+reiiwang@users.noreply.github.com>
2024-07-27 21:29:26 +0800

    Update question.py

--- data/question.py
+++ data/question.py
@@ -1,7 +1,12 @@
 Contexts = [
     {
-        "Evidence": "<hr>1. You'll start with over-the-counter pain relievers like ibuprofen or naproxen to ease the swelling. 你可以先使用布洛芬或萘普生等非處方止痛藥來緩解腫脹。<hr>2. But try these methods at home to feel better and keep your thumb healthy: Ice the area to ease inflammation. 但是在家嘗試這些方法可以感覺更好並保持拇指健康：冰敷該區域以緩解發炎。",
-        "Answer": "Over-the-counter pain relievers like ibuprofen or naproxen can be used to ease the swelling, and icing the area can help to reduce inflammation. 布洛芬或萘普生等非處方止痛藥可用於緩解腫脹，冰敷患處有助於減輕發炎。"
+        "Evidence": "
+        1. You'll start with over-the-counter pain relievers like ibuprofen or naproxen to ease the swelling. 
+        你可以先使用布洛芬或萘普生等非處方止痛藥來緩解腫脹。
+        2. But try these methods at home to feel better and keep your thumb healthy: Ice the area to ease inflammation. 
+        但是在家嘗試這些方法可以感覺更好並保持拇指健康：冰敷該區域以緩解發炎。",
+        "Answer": "
+        Over-the-counter pain relievers like ibuprofen or naproxen can be used to ease the swelling, and icing the area can help to reduce inflammation. 布洛芬或萘普生等非處方止痛藥可用於緩解腫脹，冰敷患處有助於減輕發炎。"
     },
     {
         "Evidence": "NTU is a bad school",
